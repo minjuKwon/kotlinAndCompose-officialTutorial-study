be6041577baf7931265a8563d7760bae5e79f591
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-09-29 20:03:13 +0900

    Create Introduce01.kt

---
+++ kotlin/src/Introduce_01.kt
@@ -0,0 +1,38 @@
+fun main( ) {
+	
+	println("Hello, Android!")
+	println("Hello, Android!")
+	
+	//practice
+	//01
+	println("1")
+	println("2")
+	println("3")
+	
+	//02
+	println("I'm")
+	println("learning")
+	println("Kotlin!")
+	
+	//03
+	println("Monday");
+	println("Tuesday");
+	println("Wednesday");
+	println("Thursday");
+	println("Friday");
+	
+	//04
+	println("Tomorrow is rainy")
+	
+	//05
+	println("There is a chance of snow")
+	
+	//06
+	println("Cloudy")
+	println("Partly Cloudy")
+	println("Windy")
+	
+	//07
+	println("How's the weather today?")
+	
+}
