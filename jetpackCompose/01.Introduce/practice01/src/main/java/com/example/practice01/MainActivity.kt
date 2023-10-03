56d0533d404f3616615931af7dfef4f7ebee9e30
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-10-03 14:53:52 +0900

    Update ui composition in practice01 module

--- jetpackCompose/01.Introduce/practice01/src/main/java/com/example/practice01/MainActivity.kt
+++ jetpackCompose/01.Introduce/practice01/src/main/java/com/example/practice01/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.practice01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.practice01.ui.theme.IntroduceTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Tutorial()
                 }
             }
         }
@@ -30,17 +38,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Tutorial(modifier: Modifier = Modifier) {
+    Column {
+        Image(
+            painter = painterResource(R.drawable.bg_compose_background),
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(R.string.title),
+            fontSize=24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.paragraph1),
+            textAlign=TextAlign.Justify,
+            modifier = Modifier.padding(start=16.dp, end=16.dp)
+        )
+        Text(
+            text = stringResource(R.string.paragraph2),
+            textAlign=TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     IntroduceTheme {
-        Greeting("Android")
+        Tutorial()
     }
 }
