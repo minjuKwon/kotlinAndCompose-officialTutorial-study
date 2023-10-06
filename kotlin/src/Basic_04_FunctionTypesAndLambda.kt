24ae9c918f446832960cfaff541c91e293b3e335
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-10-06 19:46:15 +0900

    Create Basic_04_FunctionTypesAndLambda.kt

---
+++ kotlin/src/Basic_04_FunctionTypesAndLambda.kt
@@ -0,0 +1,45 @@
+fun main() {
+//	val trickFuntion=trick
+//	trick()
+//	trickFuntion()
+//	treat()
+	
+//	val coins: (Int) -> String ={ quantity->
+//		"$quantity quarters"
+//	}
+	
+//val coins: (Int) -> String ={ "$it quarters" }
+	
+//	val cupcake: (Int) -> String={
+//		"Have a cupcake!"
+//	}
+	
+	//val treatFunction=trickOrTreat(false, coins)
+	//val treatFunction=trickOrTreat(false, {"$it quarters"})
+	val treatFunction=trickOrTreat(false){"$it quarters"}
+	val trickFunction=trickOrTreat(true, null)
+	//treatFunction()
+	repeat(4){
+		treatFunction()
+	}
+	trickFunction()
+}
+
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) ->String )?): () ->Unit {
+	if(isTrick){
+		return trick
+	}else{
+		if(extraTreat!=null){
+			println(extraTreat(5))			
+		}
+		return treat
+	}
+}
+
+val trick= {
+	println("No treats!")
+}
+
+val treat: () ->Unit={
+	println("Have a treat!")
+}
