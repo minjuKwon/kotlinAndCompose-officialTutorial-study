bbf93b49db8cc4f2adb3808c9e5c3089064d170f
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-10-29 17:20:09 +0900

    Add navigation

--- jetpackCompose/04.NavigationAndArchitecture/cupcake/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ jetpackCompose/04.NavigationAndArchitecture/cupcake/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -15,6 +15,9 @@
  */
 package com.example.cupcake
 
+import android.content.Context
+import android.content.Intent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
@@ -28,23 +31,40 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.cupcake.data.DataSource.flavors
+import com.example.cupcake.data.DataSource.quantityOptions
+import com.example.cupcake.ui.OrderSummaryScreen
 import com.example.cupcake.ui.OrderViewModel
+import com.example.cupcake.ui.SelectOptionScreen
+import com.example.cupcake.ui.StartOrderScreen
+
+enum class CupCakeScreen() {
+    Start,
+    Flavor,
+    Pickup,
+    Summary
+}
 
 /**
  * Composable that displays the topBar and displays back button if back navigation is possible.
  */
 @Composable
 fun CupcakeAppBar(
+    currentScreen: CupCakeScreen,
     canNavigateBack: Boolean,
     navigateUp: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     TopAppBar(
-        title = { Text(stringResource(id = R.string.app_name)) },
+        title = { Text(currentScreen.name) },
         colors = TopAppBarDefaults.mediumTopAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer
         ),
@@ -64,19 +84,101 @@ fun CupcakeAppBar(
 
 @Composable
 fun CupcakeApp(
-    viewModel: OrderViewModel = viewModel(),
-    navController: NavHostController = rememberNavController()
+    modifier:Modifier=Modifier,
+    viewModel: OrderViewModel = viewModel()
 ) {
+    val navController=rememberNavController()
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen=CupCakeScreen.valueOf(backStackEntry?.destination?.route ?:CupCakeScreen.Start.name)
 
     Scaffold(
         topBar = {
             CupcakeAppBar(
-                canNavigateBack = false,
-                navigateUp = { /* TODO: implement back navigation */ }
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry!=null,
+                navigateUp = { navController.navigateUp()}
             )
         }
     ) { innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
+        NavHost(
+            navController=navController,
+            startDestination  = CupCakeScreen.Start.name,
+            modifier=modifier.padding(innerPadding)
+        ){
 
+            composable(route=CupCakeScreen.Start.name){
+                StartOrderScreen(
+                    quantityOptions=quantityOptions,
+                    onNextButtonClicked = {
+                        viewModel.setQuantity(it)
+                        navController.navigate(CupCakeScreen.Flavor.name)
+                    }
+                )
+            }
+
+            composable(route=CupCakeScreen.Flavor.name){
+                val context=LocalContext.current
+                SelectOptionScreen(
+                    subtotal=uiState.price,
+                    onNextButtonClicked={
+                         navController.navigate(CupCakeScreen.Pickup.name)
+                    },
+                    onCancelButtonClicked={
+                          cancelOrderAndNavigateToStart(viewModel, navController)
+                    },
+                    options=flavors.map{id->context.resources.getString(id)},
+                    onSelectionChanged={viewModel.setFlavor(it)}
+                )
+            }
+
+            composable(route=CupCakeScreen.Pickup.name){
+                SelectOptionScreen(
+                    subtotal=uiState.price,
+                    onNextButtonClicked ={
+                        navController.navigate(CupCakeScreen.Summary.name)
+                    },
+                    onCancelButtonClicked={
+                        cancelOrderAndNavigateToStart(viewModel, navController)
+                    },
+                    options=uiState.pickupOptions,
+                    onSelectionChanged={viewModel.setDate(it)}
+                )
+            }
+
+            composable(route=CupCakeScreen.Summary.name){
+                val context=LocalContext.current
+                OrderSummaryScreen(
+                    orderUiState = uiState,
+                    onCancelButtonClicked = {cancelOrderAndNavigateToStart(viewModel, navController)},
+                    onSendButtonClicked = {subject: String, summary: String ->
+                        shareOrder(context, subject=subject, summary=summary)
+                    }
+                )
+            }
+        }
     }
+}
+
+private fun shareOrder(context: Context, subject: String, summary: String) {
+    val intent=Intent(Intent.ACTION_SEND).apply{
+        type="text/plain"
+        putExtra(Intent.EXTRA_SUBJECT, subject)
+        putExtra(Intent.EXTRA_TEXT, summary)
+    }
+    context.startActivity(
+        Intent.createChooser(
+            intent,
+            context.getString(R.string.new_cupcake_order)
+        )
+    )
+}
+
+
+private fun cancelOrderAndNavigateToStart(
+    viewModel: OrderViewModel,
+    navController: NavHostController
+) {
+    viewModel.resetOrder()
+    navController.popBackStack(CupCakeScreen.Start.name, inclusive=false)
 }
