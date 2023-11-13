c9f0820f0723f0dfe3526fd234d6ae6be3a45258
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-11-13 16:08:41 +0900

    Add test

--- jetpackCompose/04.NavigationAndArchitecture/reply/app/src/main/java/com/example/reply/ui/ReplyHomeScreen.kt
+++ jetpackCompose/04.NavigationAndArchitecture/reply/app/src/main/java/com/example/reply/ui/ReplyHomeScreen.kt
@@ -95,6 +95,7 @@ fun ReplyHomeScreen(
     )
 
     if(navigationType==ReplyNavigationType.PERMANENT_NAVIGATION_DRAWER){
+        val navigationDrawerContentDescription= stringResource(id = R.string.navigation_drawer)
         PermanentNavigationDrawer(
             drawerContent = {
                 PermanentDrawerSheet(Modifier.width(dimensionResource(id = R.dimen.drawer_width))){
@@ -109,7 +110,7 @@ fun ReplyHomeScreen(
                             .padding(dimensionResource(R.dimen.drawer_padding_content))
                     )
                 }
-            }
+            },modifier=Modifier.testTag(navigationDrawerContentDescription)
         ) {
             ReplyAppContent(
                 navigationType=navigationType,
@@ -189,12 +190,14 @@ private fun ReplyAppContent(
                     )
                 }
                 AnimatedVisibility(visible = navigationType==ReplyNavigationType.BOTTOM_NAVIGATION) {
+                    val bottomNavigationContentDescription=stringResource(R.string.navigation_bottom)
                     ReplyBottomNavigationBar(
                         currentTab = replyUiState.currentMailbox,
                         onTabPressed = onTabPressed,
                         navigationItemContentList = navigationItemContentList,
                         modifier = Modifier
                             .fillMaxWidth()
+                            .testTag(bottomNavigationContentDescription)
                     )
                 }
             }
