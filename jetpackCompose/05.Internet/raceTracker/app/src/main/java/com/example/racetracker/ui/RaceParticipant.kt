afb183fb31f15770ee4e3cae5c4c2592f9d33f3b
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-11-15 20:07:32 +0900

    Add asynchronous progress

--- jetpackCompose/05.Internet/raceTracker/app/src/main/java/com/example/racetracker/ui/RaceParticipant.kt
+++ jetpackCompose/05.Internet/raceTracker/app/src/main/java/com/example/racetracker/ui/RaceParticipant.kt
@@ -15,9 +15,12 @@
  */
 package com.example.racetracker.ui
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.delay
 
 /**
  * This class represents a state holder for race participant.
@@ -29,6 +32,19 @@ class RaceParticipant(
     private val progressIncrement: Int = 1,
     private val initialProgress: Int = 0
 ) {
+
+    suspend fun run() {
+        try{
+            while(currentProgress<maxProgress){
+                delay(progressDelayMillis)
+                currentProgress += progressIncrement
+            }
+        }catch(e: CancellationException){
+            Log.e("RaceParticipant", "$name: ${e.message}")
+            throw e
+        }
+    }
+
     init {
         require(maxProgress > 0) { "maxProgress=$maxProgress; must be > 0" }
         require(progressIncrement > 0) { "progressIncrement=$progressIncrement; must be > 0" }
