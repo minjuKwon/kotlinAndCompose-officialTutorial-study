42441d176bacde9e7d7c6d7bcfb235c66de88479
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-11-15 17:49:58 +0900

    Update Main.kt

--- kotlin/Coroutine/src/main/kotlin/Main.kt
+++ kotlin/Coroutine/src/main/kotlin/Main.kt
@@ -1,7 +1,90 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.system.*
+import kotlinx.coroutines.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val time=measureTimeMillis{
+        runBlocking {
+            println("Weather forecast")
+        /*    launch {
+                printForecast()
+            }
+            launch {
+                printTemperature()
+            }*/
+
+/*            val forecast: Deferred<String> = async{
+                getForecast()
+            }
+            val temperature: Deferred<String> = async {
+                getTemperature()
+            }
+            println("${forecast.await()} ${temperature.await()}")*/
+
+         /*   try{
+                println(getWeatherReport())
+            }catch (e: java.lang.AssertionError){
+                println("Caught exception in runBlocking(): $e")
+                println("Report unavailable at this time")
+            }*/
+
+            println(getWeatherReport())
+            println("Have a good day!")
+        }
+    }
+
+    println("Execution time: ${time/1000.0} seconds")
+
+    runBlocking {
+        println("${Thread.currentThread().name} - runBlocking function")
+        launch {
+            println("${Thread.currentThread().name} - launch function")
+            withContext(Dispatchers.Default){
+                println("${Thread.currentThread().name} - withContext function")
+                delay(1000)
+                println("10 results found.")
+            }
+            println("${Thread.currentThread().name} - end of launch function")
+        }
+        println("Loading...")
+    }
+
+}
+
+suspend fun printForecast() {
+    delay(1000)
+    print( "Sunny")
+}
+
+suspend fun printTemperature() {
+    delay(1000)
+    throw AssertionError("Temperature is invalid")
+    print( "30\u00b0C")
+}
+
+suspend fun getForecast(): String {
+    delay(1000)
+    return "Sunny"
+}
+
+suspend fun getTemperature(): String {
+    delay(500)
+    throw AssertionError("Temperature is invalid")
+    return "30\u00b0C"
+}
+
+suspend fun getWeatherReport() = coroutineScope {
+    val forecast = async { getForecast() }
+  /*  val temperature = async {
+        try{
+            getTemperature()
+        }catch (e: java.lang.AssertionError){
+            println("Caught exception $e")
+            "{No temperature found}"
+        }
+    }*/
+    val temperature = async { getTemperature() }
+    delay(200)
+    temperature.cancel()
+    //"${forecast.await()} ${temperature.await()}"
+    "${forecast.await()}"
 }
