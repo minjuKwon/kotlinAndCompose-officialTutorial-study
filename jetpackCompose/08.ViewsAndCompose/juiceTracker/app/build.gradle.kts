b06d68fe22b8d7bd0ea600ff6fe09cc1960a10dc
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-12-04 19:57:34 +0900

    Add fragment from view

--- jetpackCompose/08.ViewsAndCompose/juiceTracker/app/build.gradle.kts
+++ jetpackCompose/08.ViewsAndCompose/juiceTracker/app/build.gradle.kts
@@ -53,7 +53,12 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        compose = true
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+
 }
 
 dependencies {
@@ -72,4 +77,12 @@ dependencies {
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     implementation("com.google.android.material:material:1.9.0")
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    // other dependencies
+    // Compose
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.material3:material3")
+    implementation("com.google.accompanist:accompanist-themeadapter-material3:0.28.0")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
