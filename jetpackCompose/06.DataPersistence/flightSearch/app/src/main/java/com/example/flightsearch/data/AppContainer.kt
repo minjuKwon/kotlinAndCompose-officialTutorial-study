158c428bf5a950fc5148f179fe5a72a46471ceb4
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2024-07-16 16:07:33 +0900

    Add preferences

--- jetpackCompose/06.DataPersistence/flightSearch/app/src/main/java/com/example/flightsearch/data/AppContainer.kt
+++ jetpackCompose/06.DataPersistence/flightSearch/app/src/main/java/com/example/flightsearch/data/AppContainer.kt
@@ -1,16 +1,25 @@
 package com.example.flightsearch.data
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 
 interface AppContainer{
     val flightRepository:FlightRepository
 }
 
+private const val SEARCH_PREFERENCE_NAME="search_preferences"
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name= SEARCH_PREFERENCE_NAME
+)
+
 class AppDataContainer(private val context: Context) :AppContainer {
     override val flightRepository:FlightRepository by lazy{
         OfflineFlightRepository(
             airportDao=FlightDatabase.getDatabase(context).airportDao(),
-            bookmarkDao=FlightDatabase.getDatabase(context).bookmarkDao()
+            bookmarkDao=FlightDatabase.getDatabase(context).bookmarkDao(),
+            dataStore = context.dataStore
         )
     }
 }
--- jetpackCompose/06.DataPersistence/flightSearch/app/src/main/java/com/example/flightsearch/data/OfflineFlightRepository.kt
+++ jetpackCompose/06.DataPersistence/flightSearch/app/src/main/java/com/example/flightsearch/data/OfflineFlightRepository.kt
@@ -1,11 +1,26 @@
 package com.example.flightsearch.data
 
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
 
 class OfflineFlightRepository(
     private val airportDao: AirportDao,
-    private val bookmarkDao: BookmarkDao
+    private val bookmarkDao: BookmarkDao,
+    private val dataStore: DataStore<Preferences>
 ) :FlightRepository {
+
+    private companion object{
+        val SEARCH_WORD= stringPreferencesKey("search_word")
+        const val TAG="SearchPreferencesOfflineRepo"
+    }
 
     override fun getAirportStream(keyword: String): Flow<Airport>
     =airportDao.getAirport(keyword)
@@ -21,5 +36,23 @@ class OfflineFlightRepository(
 
     override suspend fun insertBookmarkData(bookmark: Bookmark)
     =bookmarkDao.insertBookmark(bookmark)
+
+    suspend fun saveSearchWordPreference(searchWord:String){
+        dataStore.edit{ preferences->
+            preferences[SEARCH_WORD]=searchWord
+        }
+    }
+
+    val searchWord:Flow<String> = dataStore.data
+        .catch {
+            if(it is IOException){
+                Log.e(TAG,"Error reading preferences $it")
+                emit(emptyPreferences())
+            }else{
+                throw it
+            }
+        }.map { preferences->
+            preferences[SEARCH_WORD]?:""
+        }
 
 }
